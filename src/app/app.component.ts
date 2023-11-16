8700f7af29daa1b17eee3a506ae5dcf6112dedc2
KamilTheKoder <kamilkazmierczak0611@gmail.com>
2023-11-16 16:26:37 +0100

    added some stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicesService } from './services/services.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'weatherApp';
+
+  constructor(private WeatherService: ServicesService) {
+
+  }
+
+  ngOnInit():void {
+    this.WeatherService.getWeatherData('Warsaw')
+    .subscribe({
+      next: (response) => {
+        console.log(response);
+      }
+    });
+  }
 }
